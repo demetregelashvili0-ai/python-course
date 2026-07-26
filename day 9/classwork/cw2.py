856867456954dbff4fdca97d3fa9f26e048d5160
demetregelashvili0-ai <demetregelashvili0@gmail.com>
2026-07-25 18:51:05 -0700

    commit

---
+++ day 9/classwork/cw2.py
@@ -0,0 +1,5 @@
+#2) მომხმარებელს შემოატანინე ორი რიცხვი (num1, num2), შეამოწმე თუ num1 მეტია num2ზე გამოვიდეს "პირველი რიცხვი მეტია",
+num1 = int(input("enter a number"))
+num2 = int(input("enter a second number"))
+if num1 > num2:
+    print("true")
---
+++ day 9/classwork/cw3.py
@@ -0,0 +1,4 @@
+#3) მომხმარებელს კიღხე რა არის სწორი რიცხვი. თუ რიცხვი უდრის 777 დაბეჭდოს "რიცხვი გამოცნობილია"
+password = int(input("enter password"))
+if password == 777:
+    print("you can enter")
