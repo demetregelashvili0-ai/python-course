5fc1b68d58851ebafb597e1d828bc0d2d335ff9f
demetregelashvili0-ai <demetregelashvili0@gmail.com>
2026-08-08 14:27:24 +0400

    commit

---
+++ day 12/classwork/cw4.py
@@ -0,0 +1,6 @@
+#4. მომხმარებელ შემოატანინეთ პაროლი, სანამ პაროლი არ დაემთხვევა 102110 მანამდე ისე თავიდან შეიყვანოს მომხმარებელმა პაროლი
+password = ''
+
+while password != 11222:
+    password = (input("enter your password"))
+    print("password is coreqt")
--- prtoeqti/p.py
+++
@@ -1,8 +0,0 @@
-age = 14
-if age > 18:
-    if age > 65:
-     print("პენსიონერი")
-    else:
-       print("ზრდასრული")
-else:
-   print("ბავსჰვი")
