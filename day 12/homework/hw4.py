8324c6badf6df0baba0d96a4b75a6cc6528b89b5
demetregelashvili0-ai <demetregelashvili0@gmail.com>
2026-08-10 11:05:16 +0400

    commit

--- day 12/homework/hw4.py
+++ day 12/homework/hw4.py
@@ -1,5 +1,6 @@
 #4. მომხმარებელს შემოატანინეთ რიცხვი და while loop-ის გამოყენებით გამოიტანეთ 1-დან ამ რიცხვამდე ყველა რიცხვი.
 num = int(input("enter a number"))
-while num >= 1:
-    print(num)
-    num -= 1
+i = 1
+while i <= num:
+    print(i)
+    i += 1
