4a77edf6844a55b6ccb0dc800c9e3481ad5ed2ae
demetregelashvili0-ai <demetregelashvili0@gmail.com>
2026-08-30 04:30:29 +0400

    commit

---
+++ day 18/homework/hw7.py
@@ -0,0 +1,10 @@
+#7. Nested for loop-ის გამოყენებით შექმენით 5x5 კვადრატი, მაგრამ მესამე ვარსკვლავი არ დაბეჭდოთ. გამოიყენეთ continue.
+
+
+
+for i in range(5):
+    for j in range(5):
+        if j == 2:
+            continue
+        print("*", end=" ")
+    print()
